c9bd6729e9af209b71175213f993b74e42751680
Kumar Utkarsh <kumar.cs01032@gmail.com>
2025-08-23 21:00:23 +0530

    added url

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ origins = [
     "http://localhost:5173",  # Your local frontend development server
     "http://localhost:3000",  # A common alternative local port
     "https://misinformation-combater-frontend.vercel.app", # <-- REPLACE THIS
+    "https://misinformation-combater-frontend-386097269689.europe-west1.run.app"
 ]
 
 # NEW: Add the CORSMiddleware to the application
